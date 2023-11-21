4b40cade919def27f78f69530ddeb898ab99ef14
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-21 10:14:40 +0900

    correction const apiKey typo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Unsplash API
 const count = 10;
-const apiKey = "API KEY";
+const apiKey = "API_KEY";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Get photos from API
