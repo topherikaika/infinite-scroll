c8780bc37a23c2ffe24bd137f7bc1e87d30bc9ae
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-21 10:11:38 +0900

    create async function getPhotos

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 const count = 10;
 const apiKey = "API KEY";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error here
+  }
+}
