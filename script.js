0786633675cf9c7d0ccf51dc8289852e7d8d4b34
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-22 06:42:29 +0900

    comment out item.setAttributes in displayPhotos function

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function displayPhotos() {
   photosArray.forEach(photo => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    // item.setAttribute("href", photo.links.html);
+    //     item.setAttribute("target", "_blank");
     // Create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    // img.setAttribute("src", photo.urls.regular);
+    //     img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
     // Put <img> inside the <a>, then put both inside imageContainer element
     item.appendChild(img);
     imageContainer.appendChild(item);
