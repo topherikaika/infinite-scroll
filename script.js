d7cfc80b97c62469271dd6f560749b08c1a7d652
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-22 06:24:37 +0900

    remove console log

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ async function getPhotos() {
   try {
     const response = await fetch(apiURL);
     photosArray = await response.json();
-    console.log(photosArray);
   } catch (error) {
     // Catch error here
   }
