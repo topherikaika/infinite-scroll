8f42bd6857232d68d225f1ae1aa9bfbf116025b2
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-22 06:36:52 +0900

    create displayPhotos function to run function for each object in photosArray. Create <a> element and set attributes of 'href' and 'target'. Create <img> element and set 'src', 'alt' and 'title' attributes. Nest <img> inside <a> element and put both in inside imageContainer element

--- script.js
+++ script.js
@@ -8,11 +8,30 @@ const count = 10;
 const apiKey = "akFpmHkI0LL4g6N5x9M49fItduJ092SHlvOv_u9hx_s";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach(photo => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+    // Create <img> for photo
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    // Put <img> inside the <a>, then put both inside imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 // Get photos from API
 async function getPhotos() {
   try {
     const response = await fetch(apiURL);
     photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
     // Catch error here
   }
