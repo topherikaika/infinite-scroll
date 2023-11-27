22be0647ddb21f4e111907848e81d3e4b543b3c7
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-28 08:16:27 +0900

    create window event listener to console log scrolls

--- script.js
+++ script.js
@@ -48,6 +48,11 @@ async function getPhotos() {
   }
 }
 
+//Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  console.log("scrolled");
+});
+
 // On Load
 
 getPhotos();
