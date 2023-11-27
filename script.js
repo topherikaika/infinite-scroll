c83d5b81081ad8b8a6d45dda67404ec13f98e602
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-11-28 08:21:06 +0900

    create if statement to trigger call getPhotos when window standards vary

--- script.js
+++ script.js
@@ -50,7 +50,10 @@ async function getPhotos() {
 
 //Check to see if scrolling near bottom of page, Load More Photos
 window.addEventListener("scroll", () => {
-  console.log("scrolled");
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    getPhotos();
+    console.log("load more");
+  }
 });
 
 // On Load
